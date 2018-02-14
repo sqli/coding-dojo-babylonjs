412eb7b533fcd11a41f28f2ba40ece6f0d49a4b5
Vincent Roth <vroth@sqli.com>
2018-02-14 18:11:22 +0100

    Step 3

--- src/app/map.js
+++ src/app/map.js
@@ -4,11 +4,25 @@ class Map {
         this.initScene(engine);
 
         let light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this.scene);
+        light.intensity = 0.3;
+
+        let spotLight = new BABYLON.SpotLight('spotLight1', new BABYLON.Vector3(0, 20, 0), new BABYLON.Vector3(0, -1, 0), 1.2, 24, this.scene);
+        let shadowGenerator = new BABYLON.ShadowGenerator(2048, spotLight);
+        shadowGenerator.usePoissonSampling = true;
+
+        let materials = new Materials(this.scene);
 
         let sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, this.scene);
         sphere.position.y = 3;
 
-        let ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, this.scene);;
+        sphere.scaling.x = -1;
+        sphere.scaling.y = -1;
+        sphere.material = materials.sphereMaterial;
+        shadowGenerator.getShadowMap().renderList.push(sphere);
+
+        let ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, this.scene);
+        ground.material = materials.groundMaterial;
+        ground.receiveShadows = true;
     }
 
     initScene(engine) {
--- src/app/materials.js
+++ src/app/materials.js
@@ -1,7 +1,11 @@
 class Materials {
 
     constructor (scene) {
+        this.groundMaterial = new BABYLON.StandardMaterial('groundTexture', scene);
+        this.groundMaterial.diffuseTexture = new BABYLON.Texture('assets/images/nasa_moon.jpg', scene);
 
+        this.sphereMaterial = new BABYLON.StandardMaterial('sphereTexture', scene);
+        this.sphereMaterial.diffuseTexture = new BABYLON.Texture('assets/images/nasa_earth.jpg', scene);
     }
 
 }
