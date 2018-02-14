ed1cb068cae4a1faa97fcf56f7420b8b580e7c4a
Vincent Roth <vroth@sqli.com>
2018-02-14 18:12:04 +0100

    Bonus step

--- src/app/user.js
+++ src/app/user.js
@@ -6,7 +6,7 @@ class User {
     }
 
     initCamera(scene, canvas) {
-        this.camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 5, -10), scene);
+        this.camera = new BABYLON.GamepadCamera('camera', new BABYLON.Vector3(0, 5, -10), scene);
         this.camera.setTarget(BABYLON.Vector3.Zero());
         this.camera.attachControl(canvas, true);
     }
